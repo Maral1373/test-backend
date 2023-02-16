f48016fa352d4761b8e08f12ffddffea58d4f1a8
Ata Mohammadi <atasmohammadi@gmail.com>
2023-02-15 19:26:10 -1000

    get product by id

--- routes/catalogRoutes.js
+++ routes/catalogRoutes.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const Product = require('../models/Product');
+const express = require("express");
+const Product = require("../models/Product");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  Product.find({})
-    .then((foundProduct) => {
-      res.send(foundProduct);
-    });
+router.get("/", (req, res) => {
+  Product.find({}).then((foundProduct) => {
+    res.send(foundProduct);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Product.findById(id).then((foundProduct) => {
+    res.send(foundProduct);
+  });
 });
 
 module.exports = router;
