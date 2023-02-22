360af60c58ba1d222e0b799b621351f11c9de37f
Ata Mohammadi <atasmohammadi@gmail.com>
2023-02-22 07:46:11 +0100

    add response

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,72 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const Cart = require('../models/Cart');
-const { checkToken } = require('../middleware/checkToken');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Cart = require("../models/Cart");
+const { checkToken } = require("../middleware/checkToken");
 
 const router = express.Router();
 const jsonParser = bodyParser.json();
 
-router.post('/', checkToken, (req, res) => {
+router.post("/", checkToken, (req, res) => {
   const user = req.user;
   const item = {
     product: req.body.product,
-    quantity: req.body.quantity
+    quantity: req.body.quantity,
   };
 
-  Cart.findOne({ user: user })
-    .then((foundCart) => {
-      if (foundCart) {
-        let products = foundCart.items.map((item) => item.product + '');
-        if (products.includes(item.product)) {
-          Cart.findOneAndUpdate({
+  Cart.findOne({ user: user }).then((foundCart) => {
+    if (foundCart) {
+      let products = foundCart.items.map((item) => item.product + "");
+      if (products.includes(item.product)) {
+        Cart.findOneAndUpdate(
+          {
             user: user,
             items: {
-              $elemMatch: { product: item.product }
-            }
+              $elemMatch: { product: item.product },
+            },
           },
-            {
-              $inc: { 'items.$.quantity': item.quantity }
-            })
-            .exec()
-            .then(() => res.end());
-        } else {
-          foundCart.items.push(item);
-          foundCart.save().then(() => res.end());
-        }
+          {
+            $inc: { "items.$.quantity": item.quantity },
+          }
+        )
+          .exec()
+          .then(res.send);
       } else {
-        Cart.create({
-          user: user,
-          items: [item]
-        })
-          .then(() => res.end());
+        foundCart.items.push(item);
+        foundCart.save().then(res.send);
       }
-    });
-});
-
-router.get('/', checkToken, (req, res) => {
-  Cart.findOne({ user: req.user.id })
-  .populate('items.product')
-  .exec((err, cart) => {
-    if (!cart) {
-      return res.send(null);
+    } else {
+      Cart.create({
+        user: user,
+        items: [item],
+      }).then(res.send);
     }
-
-    res.send(cart);
   });
 });
 
-router.put('/', checkToken, jsonParser, (req, res) => {
-  Cart.findById(req.body.cartId)
-    .then((foundCart) => {
-      foundCart.items = foundCart.items.filter((item) => item._id != req.body.itemId);
-      foundCart.save(() => res.end());
+router.get("/", checkToken, (req, res) => {
+  Cart.findOne({ user: req.user.id })
+    .populate("items.product")
+    .exec((err, cart) => {
+      if (!cart) {
+        return res.status(404).send(null);
+      }
+
+      res.status(200).send(cart);
     });
 });
 
-router.delete('/', checkToken, (req, res) => {
-  Cart.findByIdAndRemove(req.query.id)
-    .then(() => res.send({ status: true }))
-    .catch((err) => res.send({ status: false, error: err }));
+router.put("/", checkToken, jsonParser, (req, res) => {
+  Cart.findById(req.body.cartId).then((foundCart) => {
+    foundCart.items = foundCart.items.filter(
+      (item) => item._id != req.body.itemId
+    );
+    foundCart.save(res.send);
+  });
+});
+
+router.delete("/", checkToken, (req, res) => {
+  Cart.findByIdAndRemove(req.query.id).then(res.send).catch(res.send);
 });
 
 module.exports = router;
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const User = require('../models/User');
-const { checkToken } = require('../middleware/checkToken');
+const express = require("express");
+const User = require("../models/User");
+const { checkToken } = require("../middleware/checkToken");
 
 const router = express.Router();
 
-router.post('/', checkToken, (req, res) => {
-  User.findById(req.user.id)
-    .then((foundUser) => {
-      foundUser.orders = foundUser.orders.concat(req.body.order);
-      foundUser.save(() => res.end());
-    });
+router.post("/", checkToken, (req, res) => {
+  User.findById(req.user.id).then((foundUser) => {
+    foundUser.orders = foundUser.orders.concat(req.body.order);
+    foundUser.save(res.send);
+  });
 });
 
 module.exports = router;
